3947676e14e1ef7799532ab884fbd91957c7de6d
Brandon McGhee <bmcghee39@gmail.com>
2020-10-08 09:17:36 -0400

    no success in positioning

---
+++ greybuiltHomesFE/src/components/MobileMenu.vue
@@ -0,0 +1,42 @@
+<template>
+          <div class="mobile-menu">
+            <router-link tag="div" :to="{name: 'Project'}"><i class="mobile-item fas fa-hammer"></i></router-link>
+            <router-link tag="div" :to="{name: 'About'}"><i class="mobile-item fas fa-user"></i></router-link>
+            <router-link tag="div" :to="{name: 'Home'}"><i class="mobile-item fas fa-home"></i></router-link>
+            <router-link tag="div" :to="{name: 'Testimonial'}"><i class="mobile-item fas fa-book-open"></i></router-link>
+            <router-link tag="div" :to="{name: 'Contact'}"><i class="mobile-item fas fa-id-card"></i></router-link>
+        </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+.mobile-menu{
+  display: none;
+}
+  @media screen and (max-width: 500px) {
+
+    .mobile-menu {
+        display:flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        height: 120px;
+        background-color: #0C1B33;
+        color: #fff;
+
+    }
+   .mobile-item {
+
+        font-size: 2em;
+    }
+    }
+</style>
--- greybuiltHomesFE/src/components/Project.vue
+++ greybuiltHomesFE/src/components/Project.vue
@@ -1,46 +1,48 @@
 <template>
-  <div>
-    <app-header></app-header>
-    <h1>Projects</h1>
-    <div class="project-container">
-      <div class="project" v-for="project in sortedProjects">
-        <img  :src="project.images" alt="">
-      </div>
+    <div>
+        <app-header></app-header>
+        <h1>Projects</h1>
+        <div class="project-container">
+
+            <div class="project" v-for="project in projects" v-if="project.showProject">
+                <img :src="project.images" alt="">
+            </div>
+
+        </div>
+        <app-mobile></app-mobile>
     </div>
-  </div>
 </template>
 
 <script>
 import Header from './Header'
+import MobileMenu from './MobileMenu'
 export default {
-  components: {'app-header': Header},
-  data(){
-    return{
-      projects: {},
+    components: {
+        'app-header': Header,
+        'app-mobile': MobileMenu
+    },
+    data() {
+        return {
+            projects: {},
 
+        }
+    },
+    computed: {
+        sortedProjects() {
+            if (this.projects.length > 0) {
+
+            };;
+        }
     }
-  },
- computed: {
-   sortedProjects(){
-     if(this.projects.length > 0){
-       this.projects.forEach(p => {
-         if(!p.showProject)
-         {
-           let pos = this.projects.indexOf(p);
-           this.projects.splice(pos,1);
-         }
-         return this.projects;
-       });;
-     }
-   }
- }
 
-  , created: function() {
-    axios.get('/static/projects.json').then(response => {
-      this.projects= response.data.projects;
-      console.log(this.projects);
-    });
-  }
+
+    ,
+    created: function() {
+        axios.get('/static/projects.json').then(response => {
+            this.projects = response.data.projects;
+            console.log(this.projects);
+        });
+    }
 }
 </script>
 
@@ -48,29 +50,20 @@ export default {
 $container: #B3B6B7;
 $shadowGrey: #54595f;
 $background: #0C1B33;
-
-@media screen and (max-width: 500px)
-{
-  .project-container{
-    display: grid;
-    grid-template-columns: auto auto;
-    grid-template-rows: 25% 25%;
-    height: 100vh;
-    max-width: 100vw;
-    background-color: $container;
-
-      .project {
-
-
-
-        img{
-
-          height:auto;
-          max-width: 50vw;
+@media screen and (max-width: 500px) {
+    .project-container {
+        position: relative;
+        height: 80%;
+        max-width: 100vw;
+        overflow: scroll;
+        overflow-x: auto;
+        top: 15%;
+        img {
+            border-radius: 12px;
+            height: 200px;
+            width: 250px;
         }
-
     }
-  }
-
+    .project {}
 }
 </style>
