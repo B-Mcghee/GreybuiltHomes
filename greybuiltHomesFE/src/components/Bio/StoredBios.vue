996821f9740dac58f8cff6f47d2e5066b52e1b3f
Brandon McGhee <bmcghee39@gmail.com>
2020-10-31 09:28:23 -0400

    first commit

--- greybuiltHomesFE/src/components/Bio/StoredBios.vue
+++ greybuiltHomesFE/src/components/Bio/StoredBios.vue
@@ -1,8 +1,16 @@
 <template>
     <div>
-        <div  class="bios-container" >
+        <div class="bios-container">
             <div class="bio-control" v-for="bio in bios" :key="bio.BioId">
-                <Bio :bio = bio />
+                <div class="bio-individual">
+                    <base-card>
+                        <img :src="bio.Image" alt="">
+                        <h1>{{bio.FirstName}} {{bio.LastName}}</h1>
+                        <h3>{{bio.Position}}</h3>
+                        <p>{{bio.Description}}</p>
+                    </base-card>
+                </div>
+                <!-- <Bio :bio = bio /> -->
             </div>
 
         </div>
@@ -14,7 +22,7 @@ import Bio from './Bio'
 export default {
     props: ['bios'],
     components: {
-      Bio
+        Bio
     }
 }
 </script>
